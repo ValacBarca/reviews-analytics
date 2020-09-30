a98e5f08359e58fa05001e6738c31762364fc751
Mark Hsu <valac.barca@gmail.com>
2020-09-30 15:01:13 +0800

    add filter

--- read.py
+++ read.py
@@ -13,3 +13,18 @@ for d in date:
 	sum_len = sum_len + len(d)
 print("留言平均長度為",sum_len/ len(date))
 
+#篩選出少於一百字的筆數
+new = []
+for d in date:
+	if  len(d) < 100:
+		new.append(d)
+print("一共有",len(new),"留言長度小於100")
+
+#篩選含有good的評論
+good = [] 
+for d in new:
+	if "good" in d:
+		good.append(d)
+print("一共有",len(good),"則留言提到good")		
+
+
