f7c150d597b9bb0cc62d1115382bc867eb5d2325
Mark Hsu <valac.barca@gmail.com>
2020-09-30 12:49:00 +0800

    calc avg len of reviews

--- read.py
+++ read.py
@@ -6,5 +6,10 @@ with open ("reviews.txt", "r") as f:
 		count += 1
 		if count % 1000 == 0:
 		  print(len(date))
+print("檔案讀取完了,共有", len(date), "筆資料。")
 
+sum_len = 0
+for d in date:
+	sum_len = sum_len + len(d)
+print("留言平均長度為",sum_len/ len(date))
 
